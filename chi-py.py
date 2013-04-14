e1e631108a664945fa2e70876b231ab8eaa3a41c
Kenneth Lin <kenlin@berkeley.edu>
2013-04-13 19:08:39 -0700

    lolololo python executing in chinese

--- chi-py.py
+++ chi-py.py
@@ -1,24 +1,34 @@
 # -*- coding: utf-8 -*-
+# from __future__ import unicode_literals
 import subprocess, re, sys, io
 
 python = subprocess.Popen(["python"],
                           stdin = subprocess.PIPE,
-                          stdout = subprocess.PIPE,
-                          bufsize=1,
-                          close_fds = True)
+                          stdout = subprocess.PIPE)
 
 dictionary = {
-    '如果' : 'if',
-    '否则' : 'else'
+    u'如果' : 'if',
+    u'否则' : 'else',
+    u'直到' : 'while',
+    u'用' : 'with',
+    u'是' : 'is',
+    u'加入' : 'import',
+    u'不' : 'not',
+    u'于' : 'in',
+    u'在' : 'in',
+    u'范围' : 'range'
     }
 
+dictionary = { re.compile(r'\b%s\b' % k, flags = re.UNICODE) : v \
+                   for k, v in dictionary.items() }
+
 while True:
-    line = sys.stdin.readline()
+    line = sys.stdin.readline().decode("utf-8")
     if not line:
         break
     for chinese, english in dictionary.items():
         line = re.sub(chinese, english, line)
-    print >>python.stdin, line
+    print >>python.stdin, line.encode('utf-8')
 
 python.stdin.close()
 print python.stdout.read(),
