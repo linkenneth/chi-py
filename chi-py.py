cd9c0a353f02608101f56f7b26a4e3a7c7c828ba
Kenneth Lin <kenlin@berkeley.edu>
2013-04-13 20:26:16 -0700

    chinese variables workkk

--- chi-py.py
+++ chi-py.py
@@ -1,33 +1,55 @@
 # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
 import subprocess, re, sys, io
+from collections import OrderedDict
 
 python = subprocess.Popen(["python"],
                           stdin = subprocess.PIPE,
                           stdout = subprocess.PIPE)
 
-dictionary = {
-    u'如果' : 'if',
-    u'否则' : 'else',
-    u'直到' : 'while',
-    u'用' : 'with',
-    u'是' : 'is',
-    u'加入' : 'import',
-    u'不' : 'not',
-    u'于' : 'in',
-    u'在' : 'in',
-    u'范围' : 'range'
-    }
+dictionary = OrderedDict([
+    [ ur'如果', 'if' ],
+    [ ur'否则', 'else' ],
+    [ ur'直到', 'while' ],
+    [ ur'用', 'with' ],
+    [ ur'是', 'is' ],
+    [ ur'加入', 'import' ],
+    [ ur'不', 'not' ],
+    [ ur'于', 'in' ],
+    [ ur'在', 'in' ],
+    [ ur'范围', 'range' ],
+    [ ur'定义', 'def' ],
+    [ ur'一箭双雕\((.*?),\s*(.*?)\)',
+      "__chi_py_p = __chi_py_Pool(5); __chi_py_p.map(\\1, \\2)" ]
+])
 
-dictionary = { re.compile(r'\b%s\b' % k, flags = re.UNICODE) : v \
+dictionary = { re.compile(k, flags = re.UNICODE) : v \
                    for k, v in dictionary.items() }
+
+CHINESE_REGEX = re.compile(u'[\u4e00-\u9fff\u3400-\u4dff]')
+chi_idents = {}
+
+v = 0
+
+SETUP = [ "# -*- coding: utf-8 -*-",
+          "from multiprocessing import Pool as __chi_py_Pool" ]
+
+for line in SETUP:
+    print >>python.stdin, line
 
 while True:
     line = sys.stdin.readline().decode("utf-8")
+    print line,
     if not line:
         break
     for chinese, english in dictionary.items():
         line = re.sub(chinese, english, line)
+    idents = re.findall(CHINESE_REGEX, line)
+    for ident in idents:
+        if ident not in chi_idents:
+            chi_idents[ident] = "__chi_py_v" + str(v)
+            v += 1
+        line = line.replace(ident, chi_idents[ident])
+    print line
     print >>python.stdin, line.encode('utf-8')
 
 python.stdin.close()
---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+加入 time
+
+s = 1
+如果 s == 0:
+    s = 3
+    print s
+否则:
+    print 'fuckk'
+
+i = 0
+直到 i < 100:
+    print i
+    i += 1
